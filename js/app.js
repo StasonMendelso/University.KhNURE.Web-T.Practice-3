f55e62e387c0feca3d2339bd9a1644237a575b83
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-04-25 08:31:05 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function reverseNumber(num) {
+
+}
+
+function forEach(arr, func) {
+
+}
+
+function map(arr, func) {
+
+}
+
+function filter(arr, func) {
+
+}
+
+function getAdultAppleLovers(data) {
+
+}
+
+function getKeys(obj) {
+
+}
+
+function getValues(obj) {
+
+}
+
+function showFormattedDate(dateObj) {
+
+}
